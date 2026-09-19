9328de3ed6333579e0d0ef5a10c954b4954e63a5
Youssef Ahmed <yossefdiab07@gmail.com>
2026-09-19 21:27:15 +0300

    Complete JavaScript calculator

---
+++ master.js
@@ -0,0 +1,59 @@
+let button = document.querySelectorAll('.calculator-row button');
+let clear = document.querySelector('.calculator-row .clear');
+let del = document.querySelector('.calculator-row .delete');
+let equal = document.querySelector('.operator.equal');
+let display = document.querySelector('.display-area');
+let justCalculated = false;
+
+button.forEach((button) => {
+    button.addEventListener('click', () => {
+
+        if (button !== clear && button !== del && button !== equal) {
+            let buttonText = button.textContent;
+            if (justCalculated) {
+
+                if (!isNaN(buttonText)) {
+                    display.textContent = '';
+                }
+
+                justCalculated = false;
+            }
+
+            if (display.textContent === '0') {
+                display.textContent = '';
+            };
+
+            display.textContent += buttonText;
+        };
+
+    });
+});
+
+clear.addEventListener('click', () => {
+    display.textContent = '0';
+});
+
+del.addEventListener('click', () => {
+    display.textContent = display.textContent.slice(0, -1);
+    if (display.textContent === '') {
+        display.textContent = '0';
+    };
+});
+
+equal.addEventListener('click', () => {
+    try {
+        display.textContent = eval(display.textContent);
+        justCalculated = true;
+    } catch (error) {
+        display.textContent = 'Error';
+    }
+});
+
+document.addEventListener('keydown', (event) => {
+    const key = event.key;
+    button.forEach((btn) => {
+        if (btn.textContent === key) {
+            btn.click();
+        };
+    });
+});
